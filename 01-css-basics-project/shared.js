5bd1288cb9017bc070b67b0ff18ccc2a82f10746
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-10-02 19:49:45 -0500

    solving assignment 04

--- 01-css-basics-project/shared.js
+++ 01-css-basics-project/shared.js
@@ -1,5 +1,7 @@
 var $backdrop = document.querySelector('.backdrop');
 var $modal = document.querySelector('.modal');
+var $modalNoButton = document.querySelector('.modal__action--negative');
+
 var $selectPlanButtons = document.querySelectorAll('.plan button');
 
 $selectPlanButtons.forEach(button =>
@@ -8,3 +10,12 @@ $selectPlanButtons.forEach(button =>
         $backdrop.style.display = 'block';
     })
 );
+
+$backdrop.addEventListener('click', closeModal);
+
+$modalNoButton.addEventListener('click', closeModal);
+
+function closeModal() {
+    $modal.style.display = 'none';
+    $backdrop.style.display = 'none';
+}
