b5f26d9f3afd875e5921900b094e270590ffc1c7
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-10-04 19:00:12 -0500

    adding and removing css classes from elements with javascript

--- 01-css-basics-project/shared.js
+++ 01-css-basics-project/shared.js
@@ -8,24 +8,24 @@ var $selectPlanButtons = document.querySelectorAll('.plan button');
 
 $selectPlanButtons.forEach(button =>
     button.addEventListener('click', () => {
-        $modal.style.display = 'block';
-        $backdrop.style.display = 'block';
+        $modal.classList.add('open');
+        $backdrop.classList.add('open');
     })
 );
 
 $backdrop.addEventListener('click', () => {
-    $mobileNav.style.display = 'none';
+    $mobileNav.classList.remove('open');
     closeModal();
 });
 
 $modalNoButton.addEventListener('click', closeModal);
 
 function closeModal() {
-    $modal.style.display = 'none';
-    $backdrop.style.display = 'none';
+    $modal.classList.remove('open');
+    $backdrop.classList.remove('open');
 }
 
 $toggleButton.addEventListener('click', () => {
-    $mobileNav.style.display = 'block';
-    $backdrop.style.display = 'block';
+    $mobileNav.classList.add('open');
+    $backdrop.classList.add('open');
 });
